0cfddc172d1bd377e79b020fce82ee400e8993c4
myproj <nishashivakarkumari@gmail.com>
2017-08-22 22:56:12 +0530

    second commit

--- factorial.cpp
+++ factorial.cpp
@@ -1,7 +1,7 @@
 #include "functions.h"
 
 int factorial(int n){
-	if(n<0) std::cout<<"Please enter a positive number";
+	//if(n<0) std::cout<<"Please enter a positive number";
     if(n!=1){
 	return(n * factorial(n-1));
     }
