3e5656227db9187cda21820494b561f8b31699ae
AtharvaNijasure <nijsureatharva@gmail.com>
2017-08-22 17:16:01 +0530

    edited check function

--- factorial.cpp
+++ factorial.cpp
@@ -1,8 +1,17 @@
 #include "functions.h"
 
 int factorial(int n){
+	if(n>0){
     if(n!=1){
 	return(n * factorial(n-1));
     }
     else return 1;
+	}
+	else 
+	{ int a;
+		printf("enter +ve no \n");
+		scanf("%d",&a);
+		return factorial(a);
+	}
+
 }
