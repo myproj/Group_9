919ce7e5407bc6f5febf448d205841a5635b8b09
AtharvaNijasure <nijsureatharva@gmail.com>
2017-09-04 22:12:00 +0530

    1st branched commit

--- factorial.cpp
+++ factorial.cpp
@@ -3,16 +3,19 @@
 #include "cstdlib"
 int check(int n)
 {
-	while(n < 1)
+	while(n < 0)
 	{
 
-		printf("Give a number greater than 0\n");
-		std::cin >> n;
+		/*printf("Give a number greater than 0\n");
+		std::cin >> n;*/
+		return -1;
 	}
 	return n;
 }
 int factorial(int n){
 //<<<<<<< HEAD
+	if(n==0)
+		return 1;
 	if(n>0){
 //////////////////////////////////////////////////////////
 	if(n<0){
@@ -20,17 +23,14 @@ int factorial(int n){
 		return 0;
 	}
 	n = check(n);
+	if(n==-1)
+		return -1;
 //>>>>>>> fb329a93c1d48b8dd9cd2aa75bc35c48f224be03
     if(n!=1){
 	return(n * factorial(n-1));
     }
     else return 1;
 	}
-	else 
-	{ int a;
-		printf("enter +ve no \n");
-		scanf("%d",&a);
-		return factorial(a);
-	}
+	
 
 }
